24ae321b65a2ee5daa7f7427a7c98220046e994c
CHENGUANGTAO <1303444822@qq.com>
2016-10-08 09:34:58 +0800

    框架

---
+++ DAL/DB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace DAL
+{
+    public class DB
+    {
+        private static string strConn = ConfigurationManager.ConnectionStrings["mysystem"].ConnectionString;
+        public static DataTable GetDT(string strSQL)
+        {
+            SqlConnection conn = new SqlConnection(strConn);
+            conn.Open();
+
+            SqlDataAdapter da = new SqlDataAdapter(strSQL, conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            conn.Close();
+
+            return dt;
+        }
+    }
+}
---
+++ UI/Theone.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.master" AutoEventWireup="true" CodeFile="Theone.aspx.cs" Inherits="Theone" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+</asp:Content>
+
